9b8c4b0445f54fa54174249c5440eb96329bc287
Antonio Pereira <anthonyesany0712@gmail.com>
2023-08-20 17:53:01 -0300

    add path to asynchronous view

--- asyncviews/urls.py
+++ asyncviews/urls.py
@@ -20,5 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('api/', views.async_view),
     path('sync/', views.sync_view),
+    path('async/', views.async_view),
 ]
