83512a0b5786f6b83c14386cb6c9c9420438a542
Antonio Pereira <anthonyesany0712@gmail.com>
2023-08-20 17:34:11 -0300

    add synchronous view

---
+++ asyncviews/views.py
@@ -0,0 +1,18 @@
+from time import sleep
+import httpx
+from django.http import HttpResponse
+
+
+# Synchronous Function
+def http_call_sync():
+	for num in range(1, 6):
+		sleep(1)
+		print(num)
+	r = httpx.get("https://httpbin.org/")
+	print(r)
+
+
+# Synchronous Request
+def sync_view(request):
+	http_call_sync()
+	return HttpResponse("Blocking HTTP request")
